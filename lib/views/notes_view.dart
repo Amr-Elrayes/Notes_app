ba01d306745ef7d672b31c7270d452694b2b72e0
Amr-Elrayes <amrelrayes126@gmail.com>
2025-06-10 00:48:20 +0300

    create fetch notes method

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/Cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/constants.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../widgets/add_note_buttom_sheet.dart';
 import '../widgets/notes_view_body.dart';
 
@@ -9,26 +10,29 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showModalBottomSheet(
-                isScrollControlled: true,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                context: context,
-                builder: (context) {
-                  return AddNoteButtomSheet();
-                });
-          },
-          backgroundColor: kPrimaryColor,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: const Icon(
-            Icons.add,
-            color: Colors.black,
+    return BlocProvider(
+      create: (context) => NoteCubit(),
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  context: context,
+                  builder: (context) {
+                    return AddNoteButtomSheet();
+                  });
+            },
+            backgroundColor: kPrimaryColor,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: const Icon(
+              Icons.add,
+              color: Colors.black,
+            ),
           ),
-        ),
-        body: NotesViewBody());
+          body: NotesViewBody()),
+    );
   }
 }
